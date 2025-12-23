d8b52ac8fc1f965123d8147acbe99a6685ff09d8
Jennifer Luo <jennifer.luo@tufts.edu>
2025-12-22 19:28:23 -0500

    layout remove functions

--- aftercredits/src/app/layout.tsx
+++ aftercredits/src/app/layout.tsx
@@ -1,18 +1,11 @@
-"use client";
 import "./globals.css";
 import NavigationShell from "./components/navigation.shell";
 
-export default function PageLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const handleViewChange = (view: string) => {
-    console.log("View Changed to ", view);
-  };
-  const handleSignOut = () => {
-    console.log("Signing out...");
-  };
   return (
     <html lang="en">
       <body>
